a0d1ee327ba0c10deb8c31a8d88a4b008cfa21b7
Nissaq <2020470222@student.uitm.edu.my>
2022-07-15 21:57:44 +0800

    cncb

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname= "database";
+$dbname= "funrundatabase";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$dbname);
