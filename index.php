3e1a7a6dd65aaf39d4eb9266868379e5d113caaf
Nissaq <2020470222@student.uitm.edu.my>
2022-07-15 21:31:50 +0800

    vbb

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NYVirtual</title>
+</head>
+
+  <!--Start of Tawk.to Script-->
+  <script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/62d0cc9e7b967b1179999be4/1g7vpujfg';
+  s1.charset='UTF-8'; 
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+  </script>
+  <!--End of Tawk.to Script-->
+
+<body>
+<!DOCTYPE html>
+<html>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+
+<div class="w3-container w3-teal">
+  <h1>New Year Virtual Run</h1>
+</div>
+<img src="NYVFRR22-Banner.jpg" alt="Car" class="center" style="width:70%">
+
+<div class="w3-container">
+ 
+</div>
+<p class="login-register-text">Don't have an account? <a href="userregistration.php">Register Here</a>.</p>
+
+<p>Thank you for joining us!</p>
+</div>
+
+<div class="w3-container w3-teal">
+  <p>mardiana:)</p>
+
+</body>
+</html> 
+
+</body>
+</html>
